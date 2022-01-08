b0c34aacc54a9a02196d840617e70213e5f23055
ReqqQ <nevvgaming@gmail.com>
2022-01-08 18:06:05 +0100

    [CN-1] - crawler WP part 1 crawler TOP and middle

--- app/Console/Commands/NewsCrawler.php
+++ app/Console/Commands/NewsCrawler.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Goutte\Client as GouteClient;
 use Illuminate\Console\Command;
+use Symfony\Component\DomCrawler\Crawler;
 
 class NewsCrawler extends Command
 {
@@ -22,30 +23,80 @@ class NewsCrawler extends Command
     protected $description = '';
 
     private GouteClient $gouteClient;
+
     public function __construct(GouteClient $gouteClient)
     {
         $this->gouteClient = $gouteClient;
         parent::__construct();
     }
 
+    ///php  -dxdebug.start_with_request=yes artisan crawler:news
+    private const WP_PAGES_TYPES = [
+        'wiadomosci',
+        'sportowefakty',
+        'money',
+        'gwiazdy',
+        'pudelek',
+        'o2',
+        'abczdrowie',
+        'kobieta.wp.pl',
+        'pilot.wp.pl',
+        'kuchnia.wp.pl',
+        'gadzetomania.pl',
+        'wideo.wp.pl'
+    ];
 
     public function handle()
     {
-
-        $crawler = $this->gouteClient->request('GET','https://www.wp.pl/');
-        $result= [];
-         $crawler->filter('.sc-gm52b2-0')->each(function ($main) use (&$result){
-             $result['posts'] = $main->filter('a')->each(function ($title){
-                return [
-                    'postsTitle' => $title->text(),
-                    'link' => $title->attr('href'),
-                    'img'  =>  $title->filter('img')->attr('src')
-                ];
-             });
-             //sc-1c7s2z7-0
-//             $result[]['title'] = $main->filter('.beSrCx')->each(function ($title){
-//                return $title->text();
-//             });
+        $crawler = $this->gouteClient->request('GET', 'https://www.wp.pl/');
+        $result = [];
+        $crawler->filter('.hWPLle')->each(function ($main) use (&$result) {
+            foreach($main->filter('a') as $domElement){
+                $domElement = new Crawler($domElement);
+                $href = $domElement->attr('href');
+                if ($domElement->filter('.beSrCx')->count() > 0) {
+                    $postTitle = $domElement->filter('.beSrCx')->text();
+                } elseif ($domElement->filter('.irgSwQ')->count() > 0) {
+                    $postTitle = $domElement->filter('.irgSwQ')->text();
+                } elseif ($domElement->filter('.bpwShS')->count() > 0){
+                    $postTitle =$domElement->filter('.bpwShS')->text(); //pudelek
+                } else {
+                    $postTitle = '';
+                }
+                foreach (self::WP_PAGES_TYPES as $pageType) {
+                    if (str_contains($href, $pageType) && $postTitle!='') {
+                        $result[$pageType][$postTitle] = [
+                            'postsTitle' => $postTitle,
+                            'link' => $domElement->attr('href'),
+                            'img' => $domElement->filter('img')->count() > 0 ? $domElement->filter('img')->attr('src') : '',
+                        ];
+                    }
+                }
+            }
+        });
+        $crawler->filter('#glonews')->each(function ($main) use (&$result) {
+            foreach($main->filter('a') as $domElement){
+                $domElement = new Crawler($domElement);
+                $href = $domElement->attr('href');
+                if ($domElement->filter('.beSrCx')->count() > 0) {
+                    $postTitle = $domElement->filter('.beSrCx')->text();
+                } elseif ($domElement->filter('.irgSwQ')->count() > 0) {
+                    $postTitle = $domElement->filter('.irgSwQ')->text();
+                } elseif ($domElement->filter('.bpwShS')->count() > 0){
+                    $postTitle =$domElement->filter('.bpwShS')->text(); //pudelek
+                } else {
+                    $postTitle = '';
+                }
+                foreach (self::WP_PAGES_TYPES as $pageType) {
+                    if (str_contains($href, $pageType) && $postTitle!='') {
+                        $result[$pageType][$postTitle] = [
+                            'postsTitle' => $postTitle,
+                            'link' => $domElement->attr('href'),
+                            'img' => $domElement->filter('img')->count() > 0 ? $domElement->filter('img')->attr('src') : '',
+                        ];
+                    }
+                }
+            }
         });
         dd($result);
     }
